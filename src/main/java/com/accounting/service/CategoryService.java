33a9c2e02e3d987c84535f912dc2d7b85bc8aee1
Kadifa <Kudickadifa@gmail.com>
2024-02-05 19:30:42 -0600

    category

--- src/main/java/com/accounting/service/CategoryService.java
+++ src/main/java/com/accounting/service/CategoryService.java
@@ -3,6 +3,15 @@ package com.accounting.service;
 
 import com.accounting.dto.CategoryDTO;
 
+import java.util.List;
+
+
 public interface CategoryService {
-    CategoryDTO findById(Long  id);
+
+
+    List<CategoryDTO> readAllCategories();
+
+    CategoryDTO create(CategoryDTO categoryDTO);
+
+    CategoryDTO findById(Long id);
 }
--- src/main/java/com/accounting/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/accounting/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,55 @@
 package com.accounting.service.impl;
 
 import com.accounting.dto.CategoryDTO;
+
+import com.accounting.entity.Category;
+import com.accounting.mapper.MapperUtil;
+import com.accounting.repository.CategoryRepository;
 import com.accounting.service.CategoryService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+    private final MapperUtil mapperUtil;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository, MapperUtil mapperUtil) {
+        this.categoryRepository = categoryRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+
+    @Override
+    public List<CategoryDTO> readAllCategories() {
+        List<Category> list = categoryRepository.findAll();
+        return list.stream().map(category -> mapperUtil.convert(category, new CategoryDTO()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDTO create(CategoryDTO categoryDTO) {
+        Category newCategory = categoryRepository.findById(categoryDTO.getId()).orElseThrow();
+        Category categoryToSave = mapperUtil.convert(newCategory, new Category());
+        categoryRepository.save(categoryToSave);
+        return mapperUtil.convert(categoryToSave, new CategoryDTO());
+    }
+
     @Override
     public CategoryDTO findById(Long id) {
-        return null;
+        Category category = categoryRepository.findById(id).orElseThrow();
+        return mapperUtil.convert(category, new CategoryDTO());
+
+
+
+
+        }
+
     }
-}
+
+
+
